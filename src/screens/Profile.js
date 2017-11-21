b27e76889306fae5772f802a0c931882083e9ecb
Dhiraj Das <dhiraj.das.05@gmail.com>
2017-11-21 16:07:09 +0530

    profile screen minor ui changes

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Image, Button } from 'react-native';
 import { signOut } from '../managers/AuthManager';
 
 export default class Profile extends Component {
@@ -11,35 +11,65 @@ export default class Profile extends Component {
 
     constructor(props) {
         super(props);
-      }
+    }
 
     render() {
-        const { container, 
-            title,
-            textfield, 
-            loginContainer,
-            button 
+        const { 
+            scrollview, 
+            name, 
+            designation,
+            profileContainer,
+            profileImage 
         } = styles;
 
         return(
-            <View style={container}>
-                <Text style={title}>Profile</Text>
-            </View>
-        )
+            <ScrollView style={scrollview}>
+                <View style={profileContainer}>
+                    <View style={{justifyContent: 'center'}}>
+                        <Text style={name}>Dhiraj Das</Text>
+                        <Text style={designation}>Associate Engineer</Text>
+                    </View>
+                    <View>
+                        <Image 
+                            source={require('../../assets/img/google.png')}
+                            style={profileImage}
+                        />
+                    </View>
+                </View>
+                <Button 
+                    onPress={() => console.log('pressed')}
+                    title='Click'
+                />
+            </ScrollView>
+        );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
+    scrollview: {
         flex: 1,
         backgroundColor: 'white'
     },
-    title: {
+    profileContainer: {
+        justifyContent: 'space-between',
+        flexDirection: 'row'
+    },
+    name: {
         color: 'black',
-        paddingTop: 30,
         paddingLeft: 20,
         fontFamily: 'CircularStd-Medium',
-        fontSize: 30,
-        fontWeight: 'bold'
+        fontSize: 20
+    },
+    designation: {
+        color: 'black',
+        paddingLeft: 20,
+        fontFamily: 'CircularStd-Medium',
+        fontSize: 14
+    },
+    profileImage: {
+        height: 50,
+        width: 50,
+        marginRight: 20,
+        borderRadius: 25
     }
 });
